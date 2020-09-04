7831306e802da6b7886602878a6e1c08962ca21d
fasnimo <fasnimo@gmail.com>
2020-09-03 20:53:05 -0500

    Done.

---
+++ src/components/ButtonCounter.js
@@ -0,0 +1,28 @@
+import React, {Component} from "react"
+
+class ButtonCounter extends Component{
+    constructor(){
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return {
+                count: previousState.count + 1
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+                <h1>{this.state.count}</h1>
+                <button onClick={this.handleClick}>Click Me</button>
+            </div>
+        )
+    }
+}
+
+export default ButtonCounter
