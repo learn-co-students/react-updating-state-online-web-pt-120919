711d7bd12b568e1a028ab0d18d224f4416777701
github username <github email address>
2020-09-11 10:47:00 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//   <div>
+//     Mount Components Here
+//   </div>,
+//   document.getElementById('root')
+// );
+
+
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <div>
-    Mount Components Here
-  </div>,
-  document.getElementById('root')
-);
+ 
+import ClickityClick from './components/ClickityClick';
+ 
+ReactDOM.render(<ClickityClick />, document.getElementById('root'));
